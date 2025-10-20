8a5ac12255d58f8b9011ede1b9cd82c5c4ef3a28
Meybel.s <meybel.schulz@icloud.com>
2025-10-20 03:29:46 +0200

    höchste Temperatur anzeigen und Datei freigeben

--- MaxTemp/MaxTemp/MainWindow.xaml.cs
+++ MaxTemp/MaxTemp/MainWindow.xaml.cs
@@ -38,55 +38,70 @@ namespace MaxTemp
         /// <param name="e"></param>
         private void BtnAuswerten_Click(object sender, RoutedEventArgs e)
         {
-            //Zugriff auf Datei erstellen.
-          
-            FileStream fs = new FileStream(@"temps.csv", FileMode.Open);
-            StreamReader sr = new StreamReader(fs);
+            FileStream fs = null;
+            StreamReader sr = null;
 
-            //Anfangswert setzen, um sinnvoll vergleichen zu können.
-            double maxTemp = double.MinValue;
-            string maxZeile = "";
-            int zeilen = 0, 
-            erfolgreichGelesen = 0;
-            string zeile;
-
-
-            //In einer Schleife die Werte holen und auswerten. Den größten Wert "merken".
-            while ((zeile = sr.ReadLine()) != null)
+            try
             {
-                zeilen++;
-                int lastComma = zeile.LastIndexOf(',');
-                if (lastComma < 0)
-                    continue;
+                // Zugriff auf Datei erstellen
+                fs = new FileStream(@"temps.csv", FileMode.Open);
+                sr = new StreamReader(fs);
 
-                String tempText = zeile.Substring(lastComma + 1).Trim();
-                if (double.TryParse(tempText.Replace(',', '.'), System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double temp))
+                // Anfangswerte setzen
+                double maxTemp = double.MinValue;
+                string maxZeile = "";
+                int zeilen = 0;
+                int erfolgreichGelesen = 0;
+
+                string zeile;
+                while ((zeile = sr.ReadLine()) != null)
                 {
-                    erfolgreichGelesen++;
+                    zeilen++;
 
-                    if (temp < maxTemp)
+                    // Wert nach letztem Komma extrahieren
+                    int lastComma = zeile.LastIndexOf(',');
+                    if (lastComma < 0)
+                        continue; // keine gültige Zeile
+
+                    string tempText = zeile.Substring(lastComma + 1).Trim();
+
+                    // Versuch, den Text in eine Zahl zu konvertieren
+                    if (double.TryParse(
+                            tempText.Replace(',', '.'),
+                            System.Globalization.NumberStyles.Any,
+                            System.Globalization.CultureInfo.InvariantCulture,
+                            out double temp))
                     {
-                        maxTemp = temp;
-                        maxZeile = zeile;
-                    }
+                        erfolgreichGelesen++;
 
+                        // Prüfen, ob größer als bisheriges Maximum
+                        if (temp > maxTemp)
+                        {
+                            maxTemp = temp;
+                            maxZeile = zeile;
+                        }
+                    }
                 }
 
+                // Höchstwert anzeigen
+
+                lblAusgabe.Content = $"{maxTemp} °C\n\n, ist die höchste Temperatur";
+
+   
             }
+            catch (Exception ex)
+            {
 
-
-            //Datei wieder freigeben.
-
-
-            //Höchstwert auf Oberfläche ausgeben.
-
-            MessageBox.Show("Gleich kachelt das Programm...");
-            //kommentieren Sie die Exception aus.
-            throw new Exception("peng");
-
-
-            sr.Close();
-            fs.Close();
+                MessageBox.Show("Gleich krachelt das Programm...");
+                throw new Exception("peng");
+            }
+            finally
+            {
+                // Datei wieder freigeben
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
         }
+
     }
 }
